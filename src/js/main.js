375a2aaea12cf3fa787512dbce0b402feb5c56ef
Yakup <yakupjoraev@gmail.com>
2024-04-02 09:59:02 +0300

    added coocies

--- src/js/main.js
+++ src/js/main.js
@@ -165,3 +165,45 @@ window.addEventListener('click', (event) => {
 
 
 AOS.init();
+
+
+// Функция для установки кукиса
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Функция для получения значения кукиса по имени
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+// Проверяем, был ли уже показан пользователю кукис
+var hasSeenCookieMessage = getCookie('seenCookieMessage');
+
+if (!hasSeenCookieMessage) {
+  // Показываем сообщение о кукисах
+  document.querySelector('.cookies').style.display = 'block';
+
+  // Устанавливаем кукис о том, что сообщение было показано
+  setCookie('seenCookieMessage', 'true', 7); // кукис будет действителен в течение 7 дней
+}
+
+// Обработчик события клика по кнопке "Accept and continue"
+document.querySelector('.cookies__btn').addEventListener('click', function (e) {
+  e.preventDefault();
+  // Скрываем сообщение о кукисах после нажатия кнопки
+  document.querySelector('.cookies').style.display = 'none';
+});
